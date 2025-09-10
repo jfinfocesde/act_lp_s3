1e1438b4df38d79a8effefaa7911382d7940992b
JD0P <jospinada@cesde.net>
2025-09-10 15:38:42 -0500

    Solución ejercicios 1 al 3

--- src/main/java/com/example/Ejercicio1.java
+++ src/main/java/com/example/Ejercicio1.java
@@ -2,7 +2,29 @@ package com.example;
 
 public class Ejercicio1 {
     public static void ejercicio1() {
-        System.out.println("Ejercicio 1");
-        // tu código aquí
+
+        // Declaración de variables 
+        int x = 10;
+        int y = 5; 
+
+        // Suma 
+        int resultadoSuma = x+ y;
+        System.out.println("La suma es: " + resultadoSuma);
+
+        // Resta
+        int resultadoResta = x - y; 
+        System.out.println("La resta es: " + resultadoResta);
+
+        // Multiplicación 
+        int resultadoMultiplicacion = x*y;
+        System.out.println("La multiplicación es: " + resultadoMultiplicacion);
+
+        // División entera 
+        int resultadoDivision = x / y;
+        System.out.println("La división es: " + resultadoDivision);
+
+        // Módulo
+         int resultadoModulo = x % y;
+         System.out.println("El módulo es: " + resultadoModulo);
     }
 }
--- src/main/java/com/example/Ejercicio2.java
+++ src/main/java/com/example/Ejercicio2.java
@@ -2,7 +2,27 @@ package com.example;
 
 public class Ejercicio2 {
     public static void ejercicio2() {
-        System.out.println("Ejercicio 2");
-        // tu código aquí
+
+        // Declaración de variables tipo int
+        int a = 10;
+        int b = 3; 
+
+        // División entera 
+        int divisionEntera = a/b; 
+        System.out.println("El resultado de la división entera es: " + divisionEntera);
+
+        // División decimal 
+        double divisionDecimal = (double) a /b; 
+        System.out.println("El resultado de la división decimal es: " + divisionDecimal);
+
+        //El resultado es diferente porque en la primera se uso una variable int (enteros) y en la otra una tipo double (decimales)
+
+        // Declaración de variables tipo double
+        double x = 15.5;
+        double y = 4.2;
+
+        // División de decimales 
+        double divisionDecimales = x/y;
+        System.out.println("El restultado de la división de decimales es: " + divisionDecimales);
     }
 }
