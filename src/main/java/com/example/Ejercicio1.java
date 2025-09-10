e6699c509ac68067d322db3bc04611207a738849
gomezmadrid118-sketch <gomezmadrid118@gmail.com>
2025-09-10 16:06:06 -0500

    trabajo finalizado - Emmanuel Gómez

--- src/main/java/com/example/Ejercicio1.java
+++ src/main/java/com/example/Ejercicio1.java
@@ -4,5 +4,18 @@ public class Ejercicio1 {
     public static void ejercicio1() {
         System.out.println("Ejercicio 1");
         // tu código aquí
+        int x = 10;
+        int y = 5;
+        int resultadoSuma = x + y;
+        int resultadoResta = x - y;
+        int resultadoMultiplicación = x * y;
+        int resultadoDivisión = x / y;
+        int resultadoMódulo = x % y;
+        System.out.println("las variables son: X:" + x + "y Y:" + y);
+        System.out.println("El resultado de la suma es: " + resultadoSuma);
+        System.out.println("El resultado de la resta es: " + resultadoResta);
+        System.out.println("El resultado de la multiplicación es: " + resultadoMultiplicación);
+        System.out.println("El resultado de la división es: " + resultadoDivisión);
+        System.out.println("El resultado de la operación módulo es: " + resultadoMódulo);
     }
 }
--- src/main/java/com/example/Ejercicio8.java
+++ src/main/java/com/example/Ejercicio8.java
@@ -4,5 +4,25 @@ public class Ejercicio8 {
     public static void ejercicio8() {
         System.out.println("Ejercicio 8");
         // tu código aquí
+        int calificacion = 85;
+        if (calificacion >= 90) {
+            System.out.println("A");
+        } else if (calificacion >= 80) {
+            System.out.println("B");
+        } else if (calificacion >= 70) {
+            System.out.println("C");
+        } else if (calificacion >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+        int hora = 14;
+        if (hora <= 12) {
+            System.out.println("Buenos días");
+        } else if (hora <= 18) {
+            System.out.println("Buenas tardes");
+        } else {
+            System.out.println("Buenas noches");
+        }
     }
 }
