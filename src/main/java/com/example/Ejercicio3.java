526e704b239d8f80589f704c82583443c40ee40f
Ecatano <edwinenfer2010@gmail.com>
2025-09-10 21:33:51 -0500

    Taller terminado

--- src/main/java/com/example/Ejercicio3.java
+++ src/main/java/com/example/Ejercicio3.java
@@ -3,6 +3,14 @@ package com.example;
 public class Ejercicio3 {
     public static void ejercicio3() {
         System.out.println("Ejercicio 3");
-        // tu código aquí
+    int contador = 5;
+    System.out.println("Valor inicial: " + contador);
+    System.out.println("Pre-incremento (++contador): " + (++contador));
+    System.out.println("Post-incremento (contador++): " + (contador++));
+    System.out.println("Valor después del post-incremento: " + contador);
+    System.out.println("Pre-decremento (--contador): " + (--contador));
+    System.out.println("Post-decremento (contador--): " + (contador--));
+    System.out.println("Valor final: " + contador);
+
     }
 }
--- src/main/java/com/example/Ejercicio9.java
+++ src/main/java/com/example/Ejercicio9.java
@@ -3,6 +3,32 @@ package com.example;
 public class Ejercicio9 {
     public static void ejercicio9() {
         System.out.println("Ejercicio 9");
-        // tu código aquí
+        int a = 10;
+        int b = 20;
+        if (a == b) {
+            System.out.println("a == b : " + ("igualdada"));
+        }
+        if (a != b) {
+            System.out.println("a != b : " + ("desigualdad"));
+        }
+        if (a < b) {
+            System.out.println("a < b  : " + ("menor que"));
+        }
+        if (a < b) {
+            System.out.println("a > b  : " + ("mayor que"));
+        }
+        if (a <= b) {
+            System.out.println("a <= b : " + ("menor o igual que"));
+        }
+        if (a >= b) {
+            System.out.println("a >= b : " + ("mayor o igual que"));
+        }
+        String texto1 = "Hola";
+        String texto2 = "Hola";
+        if (texto1.equals(texto2)) {
+            System.out.println("Los textos son iguales");
+        } else {
+            System.out.println("Los textos son diferentes");
+        }
     }
 }
