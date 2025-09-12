7b8a99ec00ba80d8364164d4015bcf7219a837c0
JD0P <jospinada@cesde.net>
2025-09-12 13:47:57 -0500

    Solución ejercicios 7 a 9

--- src/main/java/com/example/Ejercicio8.java
+++ src/main/java/com/example/Ejercicio8.java
@@ -2,7 +2,31 @@ package com.example;
 
 public class Ejercicio8 {
     public static void ejercicio8() {
-        System.out.println("Ejercicio 8");
-        // tu código aquí
+
+        // Declaración de variables
+        int calificacion = 85;
+        int hora = 14;
+
+        // Determinar la letra de calificación
+        if (calificacion >= 90) {
+            System.out.println("A");
+        } else if (calificacion >= 80) {
+            System.out.println("B");
+        } else if (calificacion > 70) {
+            System.out.println("C");
+        }
+        if (calificacion >= 60) {
+            System.out.println("D");
+        } else
+            System.out.println("F");
+
+           // Saludo según la hora
+           if (hora < 12) {
+            System.out.println("Buenos días");
+           }else if (hora < 18) {
+            System.out.println("Buenas tardes");
+           }else
+           System.out.println("Buenas noches");
     }
+
 }
