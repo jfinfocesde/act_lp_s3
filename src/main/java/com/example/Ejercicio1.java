db32bd824f71e12b761e0113cd16cb01d876ed4c
Jhomilopez <jsanchez@interplay.com.co>
2025-09-20 10:15:30 -0500

    Update Ejercicio1.java

--- src/main/java/com/example/Ejercicio1.java
+++ src/main/java/com/example/Ejercicio1.java
@@ -4,5 +4,29 @@ public class Ejercicio1 {
     public static void ejercicio1() {
         System.out.println("Ejercicio 1");
         // tu código aquí
+        int x = 10;
+        int y = 5;
+
+        int resultadoSuma = x + y;
+        int resultadoResta = x - y;
+        int resultadoMultiplicacion = x * y;
+        int resultadoDivision = x / y;
+        int resultadoModulo = x % y;
+
+        //suma
+        System.out.println("Resultado de la suma: " + resultadoSuma);
+
+        //Resta
+        System.out.println("Resultado de la resta: " + resultadoResta);
+
+        //Multiplicacion
+        System.out.println("Resultado de la multiplicación: " + resultadoMultiplicacion);
+
+        //division
+        System.out.println("Resultado de la división entera: " + resultadoDivision);
+
+        //modulo
+        System.out.println("Resultado del módulo: " + resultadoModulo);
+        
     }
 }
