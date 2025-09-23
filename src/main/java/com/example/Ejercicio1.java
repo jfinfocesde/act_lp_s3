15db954b4e0e27f084505116e35dd34599776fa0
Jose Saez <joseleonardosg19@gmail.com>
2025-09-22 19:47:54 -0500

    Update Ejercicio1.java

--- src/main/java/com/example/Ejercicio1.java
+++ src/main/java/com/example/Ejercicio1.java
@@ -4,5 +4,22 @@ public class Ejercicio1 {
     public static void ejercicio1() {
         System.out.println("Ejercicio 1");
         // tu código aquí
+
+        int x = 10;
+        int y = 5;
+
+        int resultadoSuma = sumar(x + y);
+        System.out.println("La Suma de x + y : " + resultadoSuma);
+        int resultadoResta = restar(x - y);
+        System.out.println("La Resta de x - y : " + resultadoResta);
+        int resultadoMultiplicacion = multiplicar(x * y);
+        System.out.println("La Multiplicación de x * y: " + resultadoMultiplicacion);
+        int resultadoDivision = dividir(x / y);
+        System.out.println("La División de x / y: " + resultadoDivision);       
+        int resultadoModulo = modulo(x % y);
+        System.out.println("El Módulo de x % y: " + resultadoModulo);   
+
+
+
     }
 }
