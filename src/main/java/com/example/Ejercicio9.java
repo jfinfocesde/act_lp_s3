eea81166390a68bc4983d95441758159bff9a078
Jose Saez <joseleonardosg19@gmail.com>
2025-09-25 15:01:55 -0500

    Update Ejercicio9.java

--- src/main/java/com/example/Ejercicio9.java
+++ src/main/java/com/example/Ejercicio9.java
@@ -4,5 +4,23 @@ public class Ejercicio9 {
     public static void ejercicio9() {
         System.out.println("Ejercicio 9");
         // tu código aquí
+        int a = 10;
+        int b = 20;
+
+        // Comparaciones con enteros
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a != b: " + (a != b));
+        System.out.println("a < b: " + (a < b));
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a <= b: " + (a <= b));
+        System.out.println("a >= b: " + (a >= b));
+
+  
+        String texto1 = "Hola";
+        String texto2 = "Hola";
+
+        // Comparación de cadenas con equals
+        System.out.println("texto1.equals(texto2): " + texto1.equals(texto2));
     }
 }
+
