8ba6167ff0bd74862270f401cf2e2de5b3098ecc
KingAskhar <dramirez12345@cesde.net>
2025-09-26 17:47:58 -0500

    Actualizacion

--- src/main/java/com/example/Ejercicio5.java
+++ src/main/java/com/example/Ejercicio5.java
@@ -3,6 +3,18 @@ package com.example;
 public class Ejercicio5 {
     public static void ejercicio5() {
         System.out.println("Ejercicio 5");
-        // tu código aquí
+        
+        double precio = 259.0;
+        precio *= 0.85; // Aplicar un descuento del 15%
+        System.out.println("Precio con descuento del 15%: " + precio);
+
+        String mensaje = "Hola";
+        mensaje += " mundo"; // Concatenar una cadena
+        System.out.println(mensaje);
+
+        int puntos = 50;
+        puntos *= 2; // Duplicar los puntos
+        puntos += 25; // Añadir 25 puntos adicionales
+        System.out.println("Total de puntos: " + puntos);
     }
 }
--- src/main/java/com/example/Ejercicio6.java
+++ src/main/java/com/example/Ejercicio6.java
@@ -3,6 +3,27 @@ package com.example;
 public class Ejercicio6 {
     public static void ejercicio6() {
         System.out.println("Ejercicio 6");
-        // tu código aquí
+        int edad = 18;
+        if (edad >= 18) {
+            System.out.println("Eres mayor de edad.");
+        } else {
+            System.out.println("Eres menor de edad.");
+        }
+
+        double nota = 7.5;
+        if (nota >= 6.0) {
+            System.out.println("Has aprobado el examen.");
+        } else {
+            System.out.println("Has suspendido el examen.");
+        }
+        int numero = -5;
+
+        if (numero > 0) {
+            System.out.println("El número es positivo.");
+        } else if (numero < 0) {
+            System.out.println("El número es negativo.");
+        } else {
+            System.out.println("El número es cero. ");
+            }
+        }
     }
-}
